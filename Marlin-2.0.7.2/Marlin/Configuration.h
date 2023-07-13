562e8a481723befe965d535cc3b6517e3e390183
Marcelo Jara Falconi <marcelo.jara@pucp.edu.pe>
2023-07-13 12:07:30 -0500

    Dimensions of delta printer

--- Marlin-2.0.7.2/Marlin/Configuration.h
+++ Marlin-2.0.7.2/Marlin/Configuration.h
@@ -23,6 +23,8 @@
 
 #define CONFIG_EXAMPLES_DIR "Creality/Ender-3/CrealityV1"
 
+
+//Delay added to fix the display screen
 #define ST7920_DELAY_1 DELAY_NS(200) // After CLK LOW
 #define ST7920_DELAY_2 DELAY_NS(400) // After DAT
 #define ST7920_DELAY_3 DELAY_NS(200) // After CLK HIGH
@@ -669,16 +671,16 @@
 #endif
 
 // Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
-#define DELTA_PRINTABLE_RADIUS 128.0 // (mm)
+#define DELTA_PRINTABLE_RADIUS 90.0 //128.0 // (mm)
 
 // Maximum reachable area
-#define DELTA_MAX_RADIUS 135.0 // (mm)
+#define DELTA_MAX_RADIUS 110.0 // (mm)
 
 // Center-to-center distance of the holes in the diagonal push rods.
-#define DELTA_DIAGONAL_ROD 280.0 // (mm)
+#define DELTA_DIAGONAL_ROD 200.0 // (mm)
 
 // Distance between bed and nozzle Z home position
-#define DELTA_HEIGHT 392.00 // (mm) Get this value from G33 auto calibrate
+#define DELTA_HEIGHT 214.00 // (mm) Get this value from G33 auto calibrate
 
 #define DELTA_ENDSTOP_ADJ \
   {                       \
@@ -686,7 +688,7 @@
   } // Get these values from G33 auto calibrate
 
 // Horizontal distance bridged by diagonal push rods when effector is centered.
-#define DELTA_RADIUS 124.0 // (mm) Get this value from G33 auto calibrate
+#define DELTA_RADIUS 89.0 // (mm) Get this value from G33 auto calibrate
 
 // Trim adjustments for individual towers
 // tower angle corrections for X and Y tower / rotate XYZ so Z tower angle = 0
